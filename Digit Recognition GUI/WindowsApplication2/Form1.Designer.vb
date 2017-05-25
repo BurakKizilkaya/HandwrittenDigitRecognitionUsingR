56f9c1308c67c7603286f817bf7412aac78d0056
BurakKizilkaya <burakkizilkaya0@gmail.com>
2017-05-25 18:36:41 +0300

    Add files via upload

---
+++ Digit Recognition GUI/WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pbDraw = New System.Windows.Forms.PictureBox()
+        Me.cmdClear = New System.Windows.Forms.Button()
+        Me.cmdSave = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.pbDraw, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pbDraw
+        '
+        Me.pbDraw.AccessibleRole = System.Windows.Forms.AccessibleRole.None
+        Me.pbDraw.BackColor = System.Drawing.Color.Black
+        Me.pbDraw.Location = New System.Drawing.Point(131, 84)
+        Me.pbDraw.Name = "pbDraw"
+        Me.pbDraw.Size = New System.Drawing.Size(28, 28)
+        Me.pbDraw.TabIndex = 0
+        Me.pbDraw.TabStop = False
+        '
+        'cmdClear
+        '
+        Me.cmdClear.Location = New System.Drawing.Point(44, 189)
+        Me.cmdClear.Name = "cmdClear"
+        Me.cmdClear.Size = New System.Drawing.Size(75, 23)
+        Me.cmdClear.TabIndex = 1
+        Me.cmdClear.Text = "Clear"
+        Me.cmdClear.UseVisualStyleBackColor = True
+        '
+        'cmdSave
+        '
+        Me.cmdSave.Location = New System.Drawing.Point(161, 189)
+        Me.cmdSave.Name = "cmdSave"
+        Me.cmdSave.Size = New System.Drawing.Size(75, 23)
+        Me.cmdSave.TabIndex = 2
+        Me.cmdSave.Text = "Save"
+        Me.cmdSave.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(343, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(736, 413)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmdSave)
+        Me.Controls.Add(Me.cmdClear)
+        Me.Controls.Add(Me.pbDraw)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.pbDraw, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents pbDraw As System.Windows.Forms.PictureBox
+    Friend WithEvents cmdClear As System.Windows.Forms.Button
+    Friend WithEvents cmdSave As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ Digit Recognition GUI/WindowsApplication2/Form1.vb
@@ -0,0 +1,99 @@
+﻿Imports System.Drawing
+Imports System
+Imports System.IO
+Imports Excel = Microsoft.Office.Interop.Excel
+
+Public Class Form1
+
+    'creating input matrix, bitmap, and other variables for drawing
+    Dim input(28, 28) As Integer
+    Dim clr As Integer
+    Dim draw As Boolean
+    Dim drawcolor As Color = Color.White
+    Dim bmp As Bitmap
+    Dim str As String
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'populate pictureboximage property
+        bmp = New Bitmap(pbDraw.Width, pbDraw.Height)
+        pbDraw.Image = bmp
+
+    End Sub
+
+    Private Sub paintbrush(x As Integer, y As Integer)
+        'this function draws the picturebox ,2 pixels for each mouse click
+        Using g As Graphics = Graphics.FromImage(pbDraw.Image)
+            g.FillRectangle(New SolidBrush(drawcolor), New Rectangle(x, y, 2, 2))
+        End Using
+        pbDraw.Refresh()
+
+    End Sub
+
+    Private Sub pbDraw_MouseDown(sender As Object, e As MouseEventArgs) Handles pbDraw.MouseDown
+        'flag change according to mouse move
+        draw = True
+        paintbrush(e.X, e.Y)
+
+    End Sub
+
+    Private Sub pbDraw_MouseMove(sender As Object, e As MouseEventArgs) Handles pbDraw.MouseMove
+        'in every move of mouse draw flag is checked if it is true, drawing is done
+        If draw = True Then
+            paintbrush(e.X, e.Y)
+        End If
+
+    End Sub
+
+    Private Sub pbDraw_MouseUp(sender As Object, e As MouseEventArgs) Handles pbDraw.MouseUp
+        'flag change according to mouse move
+        draw = False
+
+    End Sub
+
+    Private Sub cmdClear_Click(sender As Object, e As EventArgs) Handles cmdClear.Click
+        'when the clear button is pressed, picturebox is cleared
+        bmp = New Bitmap(pbDraw.Width, pbDraw.Height)
+        pbDraw.Image = bmp
+
+    End Sub
+
+
+    Private Sub cmdSave_Click(sender As Object, e As EventArgs) Handles cmdSave.Click
+
+        'drawing to bitmap and saving image as png for checking purposes
+        pbDraw.DrawToBitmap(bmp, New Rectangle(0, 0, pbDraw.Width, pbDraw.Height))
+        bmp.Save("C:/Users/labuser/Desktop/input.png", Imaging.ImageFormat.Png)
+
+
+        'file stream for write is opened
+        Dim file As System.IO.StreamWriter
+        file = My.Computer.FileSystem.OpenTextFileWriter("C:/Users/labuser/Desktop/MNISTdatasetNEW/MNISTcsv/myinput.csv", True)
+
+        'getting pixels of drawing
+        For i = 0 To 27 'we have 28 X 28 pixel images
+            For j = 0 To 27
+                With bmp.GetPixel(j, i) 'taking each pixel
+                    clr = .R
+                    input(i, j) = clr 'populating input matix
+                    str = str & input(i, j) & ","   'creating comma delimated string to save input to csv file
+                End With
+            Next j
+        Next i
+        str = str.Substring(0, str.Length - 1)
+
+        file.WriteLine(str) 'writing input pixels to csv file
+        file.Close()
+
+
+        'printing input matrix to see content of matrix
+        For a = 0 To 27
+            For b = 0 To 27
+                Label1.Text = Label1.Text & input(a, b) & " "
+            Next
+            Label1.Text = Label1.Text & vbNewLine
+        Next
+
+    End Sub
+End Class
